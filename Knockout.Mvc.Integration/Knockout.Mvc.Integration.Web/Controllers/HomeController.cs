9a17cc3b63c7639e0958b96ab3c5468bb3593c8d
James Hughes <kouphax@gmail.com>
2010-12-13 20:28:44 +0000

    Adding first example using postJson

---
+++ Knockout.Mvc.Integration/Knockout.Mvc.Integration.Web/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Knockout.Mvc.Integration.Web.Controllers
+{
+    public class HomeController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PostJson(FormCollection form)
+        {
+            ViewBag.Example1Message = string.Format(
+                "Submitted FormCollection as firstName: {0}; lastName: {1}; username: {2};",
+                form["firstName"],
+                form["lastName"],
+                form["username"]);
+
+            return View("Index");
+        }
+    }
+}
---
+++ Knockout.Mvc.Integration/Knockout.Mvc.Integration.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Knockout.Mvc.Integration.Web.MvcApplication" Language="C#" %>
